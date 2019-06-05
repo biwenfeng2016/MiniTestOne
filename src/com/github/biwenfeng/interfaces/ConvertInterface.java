794b42c88970a08da9ecdffdc944a63c5c9e31cc
biwenfeng <13416152908@139.com>
2019-06-05 15:27:38 +0800

    ss

--- src/com/github/biwenfeng/interfaces/ConvertInterface.java
+++ src/com/github/biwenfeng/interfaces/ConvertInterface.java
@@ -4,7 +4,7 @@ public abstract class ConvertInterface {
 
 	public abstract String convert(Integer number);
 
-	public void printNumber() {
+	public void printNumber() { 
 		for (int i = 1; i <= 100; i++) { 
 			String result = convert(i);
 			System.out.println(result);
